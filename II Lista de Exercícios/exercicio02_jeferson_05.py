951928e1f23661e49bf908245c9a70bb41526a22
Jeferson Farias Calazans <calazans10@gmail.com>
2013-09-12 10:39:29 -0300

    refatorando códigos da segunda lista de exercícios

--- II Lista de Exercícios/exercicio02_jeferson_05.py
+++ II Lista de Exercícios/exercicio02_jeferson_05.py
@@ -4,31 +4,19 @@ num1 = int(raw_input('Digite o primeiro número: '))
 num2 = int(raw_input('Digite o segundo número: '))
 num3 = int(raw_input('Digite o terceiro número: '))
 
-if num1 > num2 and num1 > num3:
+if num1 >= num2 and num1 >= num3:
     maior = num1
-elif num2 > num1 and num2 > num3:
+elif num2 >= num3:
     maior = num2
-elif num3 > num2 and num3 > num1:
+else:
     maior = num3
-elif num3 == num1 and num2 == num1 and num3 == num2:
-    maior = num1
-elif num1 == num3 and num3 > num2 and num1 > num2:
-    maior = num1
-elif num1 == num2 and num1 > num3 and num2 > num3:
-    maior = num1
 
-if num1 < num2 and num1 < num3:
+if num1 <= num2 and num1 <= num3:
     menor = num1
-elif num2 < num1 and num2 < num3:
+elif num2 <= num3:
     menor = num2
-elif num3 < num2 and num3 < num1:
+else:
     menor = num3
-elif num3 == num1 and num2 == num1 and num3 == num2:
-    menor = num1
-elif num1 == num3 and num3 < num2 and num1 < num2:
-    menor = num1
-elif num1 == num2 and num1 < num3 and num2 < num3:
-    menor = num1
 
 print 'O maior número é:', maior
 print 'O menor número é:', menor
