e0fd97f6bf545ee6d8ca397db8cf37d60ff576c3
Jeferson Farias Calazans <calazans10@gmail.com>
2013-09-12 10:49:20 -0300

    refatorando exercício 7 da lista 2

--- II Lista de Exercícios/exercicio02_jeferson_07.py
+++ II Lista de Exercícios/exercicio02_jeferson_07.py
@@ -2,9 +2,12 @@
 
 metros = int(raw_input('Informe o tamanho em m² da área a ser pintada: '))
 
-litros = metros / 3
-latas = litros / 18
-preco = latas * 80.00
+latas = metros / 54
+
+if metros % 54 != 0:
+    latas += 1
+
+preco = latas * 80
 
 print 'Quantidade de latas de tinta a serem compradas:', latas
 print 'Preço total: R$ %.2f' % preco
